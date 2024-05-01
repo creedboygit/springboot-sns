96cb10b6c2edb40e8ba975b653d3b0641f2cb3cf
creedboy <polozhzh@gmail.com>
2024-05-01 23:37:28 +0900

    error code에 token 관련 오류 추가

--- src/main/java/com/valletta/sns/exception/ErrorCode.java
+++ src/main/java/com/valletta/sns/exception/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated...."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error...."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded...."),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid....");
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid...."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token....")
+    ;
 
     private final HttpStatus status;
     private final String message;
