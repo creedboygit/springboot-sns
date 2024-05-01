a9fc7058840eb239095e92765fff40adda515544
creedboy <polozhzh@gmail.com>
2024-05-01 20:25:38 +0900

    WebSecurityConfig 에 Jwt Token으로 비교하는 필터 내용 추가

--- src/main/java/com/valletta/sns/config/WebSecurityConfig.java
+++ src/main/java/com/valletta/sns/config/WebSecurityConfig.java
@@ -1,5 +1,9 @@
 package com.valletta.sns.config;
 
+import com.valletta.sns.config.filter.JwtTokenFilter;
+import com.valletta.sns.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,10 +12,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class WebSecurityConfig {
+
+    private final UserService userService;
+
+    @Value("${jwt.secret-key}")
+    private String key;
 
     private static final String[] WHITE_LIST = {
         "/api/*/users/join",
@@ -28,6 +39,7 @@ public class SecurityConfiguration {
                     .requestMatchers("/api/**").authenticated()
 //                .anyRequest().authenticated()
             )
+            .addFilterBefore(new JwtTokenFilter(key, userService), UsernamePasswordAuthenticationFilter.class)
             .sessionManagement((sessionManagement) ->
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .httpBasic(Customizer.withDefaults())
