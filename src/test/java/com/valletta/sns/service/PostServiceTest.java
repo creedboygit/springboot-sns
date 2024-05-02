20cb46c37533ce321bd604fb9bffeee38dade698
creedboy <polozhzh@gmail.com>
2024-05-03 00:30:10 +0900

    포스트 수정 시 Service Test 추가

--- src/test/java/com/valletta/sns/service/PostServiceTest.java
+++ src/test/java/com/valletta/sns/service/PostServiceTest.java
@@ -7,6 +7,8 @@ import static org.mockito.Mockito.when;
 
 import com.valletta.sns.exception.ErrorCode;
 import com.valletta.sns.exception.SnsApplicationException;
+import com.valletta.sns.fixture.PostEntityFixture;
+import com.valletta.sns.fixture.UserEntityFixture;
 import com.valletta.sns.model.entity.PostEntity;
 import com.valletta.sns.model.entity.UserEntity;
 import com.valletta.sns.repository.PostRepository;
@@ -56,5 +58,58 @@ class PostServiceTest {
 
         SnsApplicationException e = assertThrows(SnsApplicationException.class, () -> postService.create(title, body, userName));
         assertEquals(ErrorCode.USER_NOT_FOUND, e.getErrorCode());
+    }
+
+    @Test
+    void 포스트수정이_성공한경우() {
+
+        String title = "제목";
+        String body = "내용";
+        String userName = "creed";
+        Integer postId = 1;
+
+        PostEntity postEntity = PostEntityFixture.get(userName, postId);
+        UserEntity userEntity = postEntity.getUser();
+
+        when(userRepository.findByUserName(userName)).thenReturn(Optional.of(userEntity));
+        when(postRepository.findById(postId)).thenReturn(Optional.of(postEntity));
+
+        assertDoesNotThrow(() -> postService.modify(title, body, userName, postId));
+    }
+
+    @Test
+    void 포스트수정시_포스트가존재하지않는경우() {
+
+        String title = "제목";
+        String body = "내용";
+        String userName = "creed";
+        Integer postId = 1;
+
+        PostEntity postEntity = PostEntityFixture.get(userName, postId);
+        UserEntity userEntity = postEntity.getUser();
+
+        when(userRepository.findByUserName(userName)).thenReturn(Optional.of(userEntity));
+        when(postRepository.findById(postId)).thenReturn(Optional.empty());
+
+        SnsApplicationException e = assertThrows(SnsApplicationException.class, () -> postService.modify(title, body, userName, postId));
+        assertEquals(ErrorCode.POST_NOT_FOUND, e.getErrorCode());
+    }
+
+    @Test
+    void 포스트수정시_권한이없는경우() {
+
+        String title = "제목";
+        String body = "내용";
+        String userName = "creed";
+        Integer postId = 1;
+
+        PostEntity postEntity = PostEntityFixture.get(userName, postId);
+        UserEntity writer = UserEntityFixture.get("userName1", "password");
+
+        when(userRepository.findByUserName(userName)).thenReturn(Optional.of(writer));
+        when(postRepository.findById(postId)).thenReturn(Optional.of(postEntity));
+
+        SnsApplicationException e = assertThrows(SnsApplicationException.class, () -> postService.modify(title, body, userName, postId));
+        assertEquals(ErrorCode.INVALID_PERMISSION, e.getErrorCode());
     }
 }
