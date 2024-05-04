9b1670ffbf8a82338fb43bf36f2553114429e7fc
creedboy <polozhzh@gmail.com>
2024-05-04 16:24:40 +0900

    UserResponse 추가

---
+++ src/main/java/com/valletta/sns/controller/response/UserResponse.java
@@ -0,0 +1,23 @@
+package com.valletta.sns.controller.response;
+
+import com.valletta.sns.model.UserDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class UserResponse {
+
+    private Integer id;
+    private String userName;
+
+    public static UserResponse fromUserDto(UserDto userDto) {
+
+        return new UserResponse(
+            userDto.getId(),
+            userDto.getUsername()
+        );
+    }
+}
