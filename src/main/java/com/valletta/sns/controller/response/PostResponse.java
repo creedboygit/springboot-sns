335f1663720d8d917cd8ee2ad2c8229dcf00f597
creedboy <polozhzh@gmail.com>
2024-05-04 16:24:32 +0900

    PostResponse 추가

---
+++ src/main/java/com/valletta/sns/controller/response/PostResponse.java
@@ -0,0 +1,34 @@
+package com.valletta.sns.controller.response;
+
+import com.valletta.sns.model.PostDto;
+import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class PostResponse {
+
+    private Integer id;
+    private String title;
+    private String body;
+    private UserResponse userResponse;
+    private Timestamp registerdAt;
+    private Timestamp updatedAt;
+    private Timestamp deletedAt;
+
+    public static PostResponse fromPostDto(PostDto postDto) {
+
+        return new PostResponse(
+            postDto.getId(),
+            postDto.getTitle(),
+            postDto.getBody(),
+            UserResponse.fromUserDto(postDto.getUserDto()),
+            postDto.getRegisterdAt(),
+            postDto.getUpdatedAt(),
+            postDto.getDeletedAt()
+        );
+    }
+}
