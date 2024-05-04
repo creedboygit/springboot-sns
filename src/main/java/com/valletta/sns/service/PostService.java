1cae70447e4bae3f92724934a0a0646115eb58b0
creedboy <polozhzh@gmail.com>
2024-05-04 16:24:02 +0900

    포스트 수정 Service 메서드 추가

--- src/main/java/com/valletta/sns/service/PostService.java
+++ src/main/java/com/valletta/sns/service/PostService.java
@@ -2,6 +2,7 @@ package com.valletta.sns.service;
 
 import com.valletta.sns.exception.ErrorCode;
 import com.valletta.sns.exception.SnsApplicationException;
+import com.valletta.sns.model.PostDto;
 import com.valletta.sns.model.entity.PostEntity;
 import com.valletta.sns.model.entity.UserEntity;
 import com.valletta.sns.repository.PostRepository;
@@ -29,14 +30,26 @@ public class PostService {
     }
 
     @Transactional
-    public void modify(String title, String body, String userName, Integer postId) {
+    public PostDto modify(String title, String body, String userName, Integer postId) {
 
         // user find
         UserEntity userEntity = userRepository.findByUserName(userName).orElseThrow(() ->
             new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s now founded", userName)));
 
         // post exist
+        PostEntity postEntity = postRepository.findById(postId).orElseThrow(() ->
+            new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("%s not founded", postId)));
 
         // post permission
+        if (postEntity.getUser() != userEntity) {
+            throw new SnsApplicationException(ErrorCode.INVALID_PERMISSION, String.format("%s has no permission with %s", userName, postId));
+        }
+
+        PostEntity postSaveEntity = PostEntity.builder()
+            .title(title)
+            .body(body)
+            .build();
+
+        return PostDto.fromEntity(postRepository.save(postEntity));
     }
 }
