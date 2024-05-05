7292fafb9d3a815fb02d01ccd8d90b2bfa782f8f
creedboy <polozhzh@gmail.com>
2024-05-05 16:31:53 +0900

    유저 정보로 피드 목록을 가져오는 인터페이스 구현

--- src/main/java/com/valletta/sns/repository/PostRepository.java
+++ src/main/java/com/valletta/sns/repository/PostRepository.java
@@ -1,8 +1,13 @@
 package com.valletta.sns.repository;
 
 import com.valletta.sns.model.entity.PostEntity;
+import com.valletta.sns.model.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<PostEntity, Integer> {
+
+    Page<PostEntity> findAllByUser(UserEntity userEntity, Pageable pageable);
 
 }
