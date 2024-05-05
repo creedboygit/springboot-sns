a8c12093adf70d5363d19b45d423b19268ee6656
creedboy <polozhzh@gmail.com>
2024-05-05 16:31:16 +0900

    피드 목록, my 피드 목록 Controller 구현

--- src/main/java/com/valletta/sns/controller/PostController.java
+++ src/main/java/com/valletta/sns/controller/PostController.java
@@ -7,8 +7,11 @@ import com.valletta.sns.controller.response.Response;
 import com.valletta.sns.model.PostDto;
 import com.valletta.sns.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -42,5 +45,17 @@ public class PostController {
 
         postService.delete(authentication.getName(), postId);
         return Response.success();
+    }
+
+    @GetMapping
+    public Response<Page<PostResponse>> list(Pageable pageable, Authentication authentication) {
+
+        return Response.success(postService.list(pageable).map(PostResponse::fromPostDto));
+    }
+
+    @GetMapping("/my")
+    public Response<Page<PostResponse>> my(Pageable pageable, Authentication authentication) {
+
+        return Response.success(postService.my(authentication.getName(), pageable).map(PostResponse::fromPostDto));
     }
 }
