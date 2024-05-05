e82386cbcf9938dbcdbd72e3a8c082c631cacd5f
creedboy <polozhzh@gmail.com>
2024-05-05 21:50:02 +0900

    스웨거 및 정적 리소스들은 필터를 거치지 않고 security 적용 안되도록 수정

--- src/main/java/com/valletta/sns/config/WebSecurityConfig.java
+++ src/main/java/com/valletta/sns/config/WebSecurityConfig.java
@@ -7,10 +7,12 @@ import com.valletta.sns.exception.CustomAuthenticationEntryPoint;
 import com.valletta.sns.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,6 +34,16 @@ public class WebSecurityConfig {
     };
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+//        return (web) -> web.ignoring().requestMatchers("/예외처리하고 싶은 url", "/예외처리하고 싶은 url");
+        return (web) -> web.ignoring()
+            .requestMatchers("^(?!/api/).*")
+            .requestMatchers("/api/**")
+            .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**")
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
     protected SecurityFilterChain config(HttpSecurity http) throws Exception {
         return http
             .csrf(AbstractHttpConfigurer::disable) // RestAPI는 csrf 보안이 필요 없으므로 비활성화
@@ -39,12 +51,12 @@ public class WebSecurityConfig {
             .formLogin(AbstractHttpConfigurer::disable)
             .httpBasic(AbstractHttpConfigurer::disable) // RestAPI는 UI를 사용하지 않기 때문에, 기본설정을 비활성화
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**")
-                .permitAll()
+//                .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api-docs", "/api-docs/**", "/v3/api-docs/**")
+//                .permitAll()
                 .requestMatchers(WHITE_LIST)
                 .permitAll()
-                .requestMatchers("/api/**")
-                .permitAll()
+//                .requestMatchers("/api/**")
+//                .permitAll()
                 .anyRequest()
                 .authenticated()
             )
