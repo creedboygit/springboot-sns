f6c86ed25e52fb54408089a2dea2aa7a4d5f0b96
creedboy <polozhzh@gmail.com>
2024-05-06 22:00:25 +0900

    comment 테이블에 post_id로 인덱스 생성하는 코드 추가

--- src/main/java/com/valletta/sns/model/entity/CommentEntity.java
+++ src/main/java/com/valletta/sns/model/entity/CommentEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
@@ -24,7 +25,9 @@ import org.hibernate.annotations.SQLRestriction;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "\"comment\"")
+@Table(name = "\"comment\"", indexes = {
+    @Index(name = "post_id_idx", columnList = "post_id")
+})
 @SQLDelete(sql = "UPDATE \"comment\" SET deleted_at = NOW() where id = ?")
 @SQLRestriction("deleted_at is NULL")
 @Entity
