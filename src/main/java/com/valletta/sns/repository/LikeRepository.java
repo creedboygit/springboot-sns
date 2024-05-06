f741c3a7b4ad47e10e620743b6a01c5ee26f4cea
creedboy <polozhzh@gmail.com>
2024-05-06 18:36:04 +0900

    포스트에 좋아요 수를 카운팅하기 위한 인터페이스 메서드 추가

--- src/main/java/com/valletta/sns/repository/LikeRepository.java
+++ src/main/java/com/valletta/sns/repository/LikeRepository.java
@@ -3,6 +3,7 @@ package com.valletta.sns.repository;
 import com.valletta.sns.model.entity.LikeEntity;
 import com.valletta.sns.model.entity.PostEntity;
 import com.valletta.sns.model.entity.UserEntity;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,4 +11,6 @@ public interface LikeRepository extends JpaRepository<LikeEntity, Integer> {
 
 //    LikeEntity findByUserAndPost(UserEntity user, PostEntity post);
     Optional<LikeEntity> findByUserAndPost(UserEntity user, PostEntity post);
+
+    List<LikeEntity> findAllByPost(PostEntity postEntity);
 }
