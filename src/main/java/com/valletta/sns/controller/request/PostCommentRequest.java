3f2314d1058df391b6a974ea1079dffd5c6723b5
creedboy <polozhzh@gmail.com>
2024-05-06 19:23:54 +0900

    댓글 Request 생성

---
+++ src/main/java/com/valletta/sns/controller/request/PostCommentRequest.java
@@ -0,0 +1,11 @@
+package com.valletta.sns.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PostCommentRequest {
+
+    private String comment;
+}
