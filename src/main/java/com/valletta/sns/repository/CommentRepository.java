606d541d982cf2f943b6483f878f50d5d313da50
creedboy <polozhzh@gmail.com>
2024-05-06 21:59:19 +0900

    CommentRepository에 포스트별 댓글 조회하는 메서드 추가

--- src/main/java/com/valletta/sns/repository/CommentRepository.java
+++ src/main/java/com/valletta/sns/repository/CommentRepository.java
@@ -1,8 +1,12 @@
 package com.valletta.sns.repository;
 
 import com.valletta.sns.model.entity.CommentEntity;
+import com.valletta.sns.model.entity.PostEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<CommentEntity, Integer> {
 
+    Page<CommentEntity> findAllByPost(PostEntity postEntity, Pageable pageable);
 }
