e2e699e3de740002ba3fd15c388697a6f79e1d84
creedboy <polozhzh@gmail.com>
2024-05-11 19:08:58 +0900

    host, port 지정해서 redis에 접속하는 방법으로 변경

--- src/main/java/com/valletta/sns/config/RedisConfig.java
+++ src/main/java/com/valletta/sns/config/RedisConfig.java
@@ -1,13 +1,11 @@
 package com.valletta.sns.config;
 
 import com.valletta.sns.model.dto.UserDto;
-import io.lettuce.core.RedisURI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -22,21 +20,16 @@ public class RedisConfig {
     private final RedisProperties redisProperties;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-
-        RedisURI redisURI = RedisURI.create(redisProperties.getUrl());
-        RedisConfiguration configuration = LettuceConnectionFactory.createRedisConfiguration(redisURI);
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(configuration);
-        factory.afterPropertiesSet();
-        return factory;
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort()));
     }
 
     @Bean
-    public RedisTemplate<String, UserDto> userDtoRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, UserDto> userDtoRedisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
 
         RedisTemplate<String, UserDto> redisTemplate = new RedisTemplate<>();
 
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
 
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<UserDto>(UserDto.class));
