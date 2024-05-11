10d41dbea6958753fb2df9272ce42fdb760f8e11
creedboy <polozhzh@gmail.com>
2024-05-11 23:01:30 +0900

    알람을 구독하기 위한 서비스 추가

---
+++ src/main/java/com/valletta/sns/service/AlarmService.java
@@ -0,0 +1,50 @@
+package com.valletta.sns.service;
+
+import com.valletta.sns.exception.ErrorCode;
+import com.valletta.sns.exception.SnsApplicationException;
+import com.valletta.sns.repository.EmitterRepository;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class AlarmService {
+
+    private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
+    private static final String ALARM_NAME = "alarm";
+    private final EmitterRepository emitterRepository;
+
+    public void send(Integer alarmId, Integer userId) {
+        emitterRepository.get(userId).ifPresentOrElse(
+            sseEmitter -> {
+                try {
+                    sseEmitter.send(SseEmitter.event().id(alarmId.toString()).name(ALARM_NAME).data("new alarm"));
+                } catch (IOException e) {
+                    emitterRepository.delete(userId);
+                    throw new SnsApplicationException(ErrorCode.ALARM_CONNECT_ERROR);
+                }
+            },
+            () -> log.info("No emitter founded")
+        );
+    }
+
+    public SseEmitter connectAlarm(Integer userId) {
+        SseEmitter sseEmitter = new SseEmitter(DEFAULT_TIMEOUT);
+        emitterRepository.save(userId, sseEmitter);
+
+        sseEmitter.onCompletion(() -> emitterRepository.delete(userId));
+        sseEmitter.onTimeout(() -> emitterRepository.delete(userId));
+
+        try {
+            sseEmitter.send(SseEmitter.event().id("id").name(ALARM_NAME).data("connect completed"));
+        } catch (IOException e) {
+            throw new SnsApplicationException(ErrorCode.ALARM_CONNECT_ERROR);
+        }
+
+        return sseEmitter;
+    }
+}
