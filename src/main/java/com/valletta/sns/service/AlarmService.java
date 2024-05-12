1ffad1a2cdd341596d3e4ea558593820fb841b3b
creedboy <polozhzh@gmail.com>
2024-05-12 17:59:17 +0900

    kafka 구독 시 Alarm 테이블에 insert되고, 클라이언트로 알람 전송되도록 수정

--- src/main/java/com/valletta/sns/service/AlarmService.java
+++ src/main/java/com/valletta/sns/service/AlarmService.java
@@ -2,7 +2,13 @@ package com.valletta.sns.service;
 
 import com.valletta.sns.exception.ErrorCode;
 import com.valletta.sns.exception.SnsApplicationException;
+import com.valletta.sns.model.constant.AlarmType;
+import com.valletta.sns.model.dto.AlarmArgs;
+import com.valletta.sns.model.entity.AlarmEntity;
+import com.valletta.sns.model.entity.UserEntity;
+import com.valletta.sns.repository.AlarmRepository;
 import com.valletta.sns.repository.EmitterRepository;
+import com.valletta.sns.repository.UserRepository;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,18 +23,27 @@ public class AlarmService {
     private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
     private static final String ALARM_NAME = "alarm";
     private final EmitterRepository emitterRepository;
+    private final AlarmRepository alarmRepository;
+    private final UserRepository userRepository;
 
-    public void send(Integer alarmId, Integer userId) {
-        emitterRepository.get(userId).ifPresentOrElse(
+//    public void send(Integer alarmId, Integer userId) {
+    public void send(AlarmType alarmType, AlarmArgs args, Integer receiveUserId) {
+
+        UserEntity userEntity = userRepository.findById(receiveUserId).orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", receiveUserId)));
+
+        // alarm save
+        AlarmEntity alarmEntity = alarmRepository.save(AlarmEntity.of(userEntity, alarmType, args));
+
+        emitterRepository.get(receiveUserId).ifPresentOrElse(
             sseEmitter -> {
                 try {
                     log.info("alarm sended");
                     sseEmitter.send(SseEmitter.event()
-                        .id(alarmId.toString())
+                        .id(alarmEntity.getId().toString())
                         .name(ALARM_NAME)
                         .data("new alarm"));
                 } catch (IOException e) {
-                    emitterRepository.delete(userId);
+                    emitterRepository.delete(receiveUserId);
                     throw new SnsApplicationException(ErrorCode.ALARM_CONNECT_ERROR, "alarm connect error 1");
                 }
             },
